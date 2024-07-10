8a40367de7629342122f416c1979597f8f4879ca
AnggihP <anggih10@gmail.com>
2024-07-10 22:43:04 +0700

    fixing navigation

--- src/components/Channels.js
+++ src/components/Channels.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Channels() {
   return (
-    <div className="section channels p-4">
-      <h2 className="flex justify-between items-center text-lg font-bold text-almond">
+    <div className="section channels p-4 mt-10">
+      <h2 className="flex justify-between items-center text-lg font-bold text-almond ">
         Channels <span className="text-xs text-almond cursor-pointer">Browse all channels</span>
       </h2>
-      <div className="channels-grid grid grid-cols-3 md:grid-cols-2 gap-4 mt-4 text-sm">
+      <div className="channels-grid grid grid-cols-3 md:grid-cols-2 gap-4 mt-4 text-sm border-t-4 border-almond p-4">
         <div className="channel-item small bg-almond text-cordovan p-4">Google</div>
         <div className="channel-item small bg-almond text-cordovan p-4">Dribbble</div>
         <div className="channel-item small bg-almond text-cordovan p-4">Microsoft</div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,9 @@ function Header() {
   };
 
   return (
-    <header className="bg-cordovan text-almond p-4 md:flex-row md:items-center md:justify-between ">
-      <div className="flex flex-row justify-between items-center mb-4 md:mb-0 w-full md:w-auto mx-12 ">
+    <header className="bg-cordovan text-almond p-4 md:flex-row md:items-center md:justify-between">
+      <div className="flex flex-row justify-between items-center mb-4 md:mb-0 w-full md:w-auto md:mx-12">
+        <div className="text-2xl font-bold">SocialNetwork</div>
         <div className="md:hidden">
           <button onClick={toggleMobileMenu} className="text-almond">
             <img
@@ -19,23 +20,22 @@ function Header() {
             />
           </button>
         </div>
-        <div className="text-2xl font-bold">SocialNetwork</div>
         <div className="hidden md:flex items-center space-x-4 w-full md:w-auto">
-        <input
-          type="text"
-          placeholder="Find..."
-          className="p-2 bg-almond text-cordovan placeholder-pink-200 rounded-md w-full"
-        />
-        <button className="bg-almond p-2 rounded-md text-almond">🔍</button>
-        <button className="bg-almond p-2 space-x-2 rounded-md text-sm text-cordovan bg-almond">⏫ Upload</button>
-        <div className="flex items-center space-x-2 bg-almond text-cordovan rounded-md">
-          <span>👤</span>
-          <span>Waseem Arshad</span>
+          <input
+            type="text"
+            placeholder="Find..."
+            className="p-2 bg-almond text-cordovan placeholder-pink-200 rounded-md w-full"
+          />
+          <button className="bg-almond p-2 rounded-md text-almond">🔍</button>
+          <button className="bg-almond p-2 space-x-2 rounded-md text-sm text-cordovan bg-almond">⏫ Upload</button>
+          <div className="flex items-center space-x-2 bg-almond text-cordovan rounded-md">
+            <span>👤</span>
+            <span>Waseem Arshad</span>
+          </div>
         </div>
       </div>
-      </div>
 
-      <nav className="flex flex-row hidden space-x-4 md:flex  text-xl mt-4 border-t-2 border-b-2 mx-12">
+      <nav className="flex flex-row hidden space-x-4 md:flex text-xl mt-4 border-t-2 border-b-2 mx-12 p-2">
         <a href="#videos" className="hover:underline">Videos</a>
         <span>/</span>
         <a href="#people" className="hover:underline">People</a>
@@ -50,6 +50,7 @@ function Header() {
         <span>/</span>
         <a href="#channels" className="hover:underline">Channels</a>
       </nav>
+
       {/* Mobile Menu */}
       {isMobileMenuOpen && (
         <nav className="md:hidden flex flex-col space-y-2 mt-4">
